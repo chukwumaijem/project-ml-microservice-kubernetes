e8b522152bf8d750a1d54bad0e3e4e0d5fa319df
Chukwuma Zikora <chukwuma.zikora@gmail.com>
2022-07-18 08:57:55 +0100

    add ci and kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=chukwumaijem/ml-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# kubectl create deploy ml-project --image=$dockerpath
+kubectl run ml-project --image=$dockerpath --image-pull-policy=IfNotPresent
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/ml-project 8000:80
